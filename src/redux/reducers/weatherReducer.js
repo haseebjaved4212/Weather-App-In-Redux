615a080e75d5cc77869c34e8fb9f1f093e9aa01a
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2026-01-13 12:42:18 +0500

    Complete A section

--- src/redux/reducers/weatherReducer.js
+++ src/redux/reducers/weatherReducer.js
@@ -1,3 +1,6 @@
+import { FETCT_CURRENT_SUCCESS, FETCT_FORECAST_SUCCESS, FETCT_WEATHER_ERROR, FETCT_WEATHER_PENDING, FETCT_WEATHER_SUCCESS, SET_CITY } from "../constants/weatherConstants";
+
+
 const initialState = {
     city : "Karachi",
     current: null,
@@ -15,7 +18,20 @@ const initialState = {
 
 
 const weatherReducer = (state = initialState, action) => {
-    if(action.type === "")
+if(action.type === FETCT_WEATHER_PENDING){
+    
+}else if(action.type === FETCT_CURRENT_SUCCESS){
+
+}else if(action.type === FETCT_FORECAST_SUCCESS){
+
+}else if(action.type === FETCT_WEATHER_ERROR){
+
+}else if(action.type === SET_CITY){
+
+}
+else{
+    return state
+}
 };
 
 export default weatherReducer;
