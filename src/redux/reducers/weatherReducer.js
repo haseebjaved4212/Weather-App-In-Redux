34050e2e854008aba191f1896ac41a9ec96d1a3f
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2026-01-13 12:35:18 +0500

    Create Constants

---
+++ src/redux/reducers/weatherReducer.js
@@ -0,0 +1,21 @@
+const initialState = {
+    city : "Karachi",
+    current: null,
+    forecast: null,
+    loading : false,
+
+    error : null
+}
+
+
+
+
+
+
+
+
+const weatherReducer = (state = initialState, action) => {
+    if(action.type === "")
+};
+
+export default weatherReducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,7 @@
 import {createStore} from "redux";
+import weatherReducer from "./reducers/weatherReducer";
 
-const store = createStore();
+const store = createStore(weatherReducer);
 
 
 export default store;
