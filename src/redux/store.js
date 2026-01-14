8261be217dc314590091fa225e16b3796dd639a1
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2026-01-14 11:10:59 +0500

    Fix a bug

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,12 +2,11 @@ import {createStore, compose , applyMiddleware} from "redux";
 import weatherReducer from "./reducers/weatherReducer";
 import {thunk }from "redux-thunk";
 
-const reduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
     weatherReducer,
-    compose(
-        applyMiddleware(thunk),
-        reduxDevTools
+    composeEnhancers(
+        applyMiddleware(thunk)
     ));
 
 
