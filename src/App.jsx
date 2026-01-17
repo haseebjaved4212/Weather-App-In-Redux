bede29ef309aa8209d4abe5acf9e890c78620fba
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2026-01-17 08:42:47 +0500

    feat: Implement initial application structure and current weather display with Redux integration.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useDispatch, useSelector } from "react-redux"
 import { useEffect } from "react"
 import { fetchWeather } from "./redux/actions/weatherActions"
 import Search from "./components/Search"
-
+import CurrentWeather from "./components/CurrentWeather"
 const App = () => {
 
 const city = useSelector((state) => state.weatherDetails.city)
@@ -15,6 +15,7 @@ const city = useSelector((state) => state.weatherDetails.city)
   return ( 
     <div className=" p-12 flex justify-center items-center flex-col">
     <Search/>
+    <CurrentWeather/>
     </div>
   )
 }
